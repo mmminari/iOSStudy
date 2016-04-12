22455d717c4ade8fe406405f1bdafa4564901f30
김민아 <mmminari@gimminaui-macbook-pro.local>
2016-04-12 18:14:03 +0900

    study

--- TestAttributeString/TestAttributeString/ViewController.m
+++ TestAttributeString/TestAttributeString/ViewController.m
@@ -14,9 +14,110 @@
 
 @implementation ViewController
 
+
+
+
+- (IBAction)show:(UIButton *)sender {
+    
+    
+    
+    NSString *introduction = @"제 이름은 %s1 입니다. 나이는 %s2이고, 취미는 %s3 입니다.";
+    
+    
+    introduction = [introduction stringByReplacingOccurrencesOfString:@"%s1" withString:self.userName];
+    introduction = [introduction stringByReplacingOccurrencesOfString:@"%s2" withString:self.userAge];
+    introduction = [introduction stringByReplacingOccurrencesOfString:@"%s3" withString:self.userHobby];
+    
+    
+    self.finalIntro = [[NSMutableAttributedString alloc] initWithString:introduction];
+    
+    NSLog(@"final %@", self.finalIntro);
+    
+    NSRange nameRange = [introduction rangeOfString:self.userName];
+    NSRange ageRange = [introduction rangeOfString:self.userAge];
+    NSRange hobbyRange = [introduction rangeOfString:self.userHobby];
+    
+    
+    [self.finalIntro addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:NSMakeRange(nameRange.location, self.userName.length)];
+    [self.finalIntro addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:20.0f] range:NSMakeRange(nameRange.location, self.userName.length)];
+    
+    [self.finalIntro addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:16.0f] range:NSMakeRange(ageRange.location, self.userAge.length)];
+    
+    [self.finalIntro addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:16.0f] range:NSMakeRange(hobbyRange.location, self.userHobby.length)];
+    
+    
+    self.showLabel.attributedText = self.finalIntro;
+    NSLog( @"%@", self.finalIntro);
+    
+    
+
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    
+    if(self.inputName.text.length > 10 ){
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                                       message:@"exceed the input limit"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:defaultAction];
+        [self presentViewController:alert animated:YES completion:nil];
+        
+        self.inputName.text = @"";
+    }
+    
+    if(self.inputHobby.text.length > 10 ){
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                                       message:@"exceed the input limit"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:defaultAction];
+        [self presentViewController:alert animated:YES completion:nil];
+        
+        self.inputHobby.text = @"";
+    }
+    if(self.inputAge.text.length > 2 ){
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                                       message:@"exceed the input limit"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:defaultAction];
+        [self presentViewController:alert animated:YES completion:nil];
+        
+        self.inputAge.text = @"";
+    }
+    
+    
+    
+    self.userName = self.inputName.text;
+    self.userAge = self.inputAge.text;
+    self.userHobby  = self.inputHobby.text;
+    
+    
+    return YES;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.showLabel.text = @" "  ;
+    
+    
+
+    
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /*
     NSString *userName = @"김민아11111";
     NSString *templetSentence = @"내이름은 %s1 입니다.";
     
@@ -34,7 +135,7 @@
     
 
     NSString *userNameEng = @"Mina";
-    NSString *templetSentenceEng = @"니ㅏㅇ러ㅣ나ㅓㅇㄹ %s1.ㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇㄹ ㄴㅇㄹㄴㅇㄹ";
+    NSString *templetSentenceEng = @"My name is %s1";
     
     templetSentenceEng = [templetSentenceEng stringByReplacingOccurrencesOfString:@"%s1" withString:userNameEng];
 
@@ -49,15 +150,10 @@
     self.nameLabel.attributedText = korName;
     self.nameLabelEng.attributedText = engName;
     
+    */
+
     
-    
-     
-    
-    
-    
-                           
-                           
-    
+ 
     
 }
 
