916164b5121457ad5c2fbb94aaaaf94c2aafe7a3
김민아 <mmminari@gimminaui-macbook-pro.local>
2016-04-12 12:42:12 +0900

    프로젝트생성

---
+++ TestAttributeString/TestAttributeString/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAttributeString
+//
+//  Created by 김민아 on 2016. 4. 12..
+//  Copyright © 2016년 김민아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAttributeString/TestAttributeString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAttributeString
+//
+//  Created by 김민아 on 2016. 4. 12..
+//  Copyright © 2016년 김민아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
