2a99389e2289eb90f83fb19957f3ebd2939172d9
김민아 <mmminari@gimminaui-macbook-pro.local>
2016-04-25 17:44:27 +0900

    함수이름수정 컬러값계산함수수정

--- NavigationController/NavigationController/IntroUiViewController.m
+++ NavigationController/NavigationController/IntroUiViewController.m
@@ -34,7 +34,7 @@
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *alcLbInputCommentWidth;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *alcLbInputCommentHeight;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *alcLbPrivateServiceWidth;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *AlcLbPrivateServiceHeight;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *alcLbPrivateServiceHeight;
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *alcBottomMarginOfIvBottom;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *alcBottomMarginFromLbDetailToIvBottom;
@@ -60,12 +60,12 @@
     int screenWidth = [UIScreen mainScreen].bounds.size.width;
     [self setRatioOfDevicewithDevieWidth:screenWidth];
     
-    self.mainContainerView.backgroundColor = [self setColorWithRGBCode:@"ffb5bf"];
-    self.btnOk.backgroundColor = [self setColorWithRGBCode:@"4b4b4b"];
-    self.lbInputComment.textColor = [self setColorWithRGBCode:@"4b4b4b"];
-    self.lbPrivateService.textColor = [self setColorWithRGBCode:@"4b4b4b"];
-    self.lbDetail.textColor = [self setColorWithRGBCode:@"4b4b4b"];
-    self.tfInput.backgroundColor = [self setColorWithRGBCode:@"ffffff"];
+    self.mainContainerView.backgroundColor = [self getColorWithRGBCode:@"ffb5bf"];
+    self.btnOk.backgroundColor = [self getColorWithRGBCode:@"4b4b4b"];
+    self.lbInputComment.textColor = [self getColorWithRGBCode:@"4b4b4b"];
+    self.lbPrivateService.textColor = [self getColorWithRGBCode:@"4b4b4b"];
+    self.lbDetail.textColor = [self getColorWithRGBCode:@"4b4b4b"];
+    self.tfInput.backgroundColor = [self getColorWithRGBCode:@"ffffff"];
 
     self.ivBottomImage.image = [UIImage imageNamed:@"SM_logos"];
     self.ivLogoImage.image = [UIImage imageNamed:@"smtownmembership_logo@3x"];
@@ -97,53 +97,53 @@
 -(void)setRatioOfDevicewithDevieWidth:(int)width
 {
     
-    [self setWidthOfTargetWithTargetValue:self.alcLbDetailWidth.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcLbDetailHeight.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcIvLogoWidth.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcIvLogoHeight.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcTfInputWidth.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcTfInputHeight.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcBtnOkWidth.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcBtnOkheight.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcIvBottmWidth.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcIvBottomHeight.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcLbInputCommentWidth.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcLbInputCommentHeight.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcLbPrivateServiceWidth.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.AlcLbPrivateServiceHeight.constant andDevieWidth:width];
+    self.alcLbDetailWidth.constant = [self getWidthOfTargetWithTargetValue:self.alcLbDetailWidth.constant andDevieWidth:width];
+    self.alcLbDetailHeight.constant = [self getWidthOfTargetWithTargetValue:self.alcLbDetailHeight.constant andDevieWidth:width];
+    self.alcIvLogoWidth.constant = [self getWidthOfTargetWithTargetValue:self.alcIvLogoWidth.constant andDevieWidth:width];
+    self.alcIvLogoHeight.constant = [self getWidthOfTargetWithTargetValue:self.alcIvLogoHeight.constant andDevieWidth:width];
+    self.alcTfInputWidth.constant = [self getWidthOfTargetWithTargetValue:self.alcTfInputWidth.constant andDevieWidth:width];
+    self.alcTfInputHeight.constant = [self getWidthOfTargetWithTargetValue:self.alcTfInputHeight.constant andDevieWidth:width];
+    self.alcBtnOkWidth.constant = [self getWidthOfTargetWithTargetValue:self.alcBtnOkWidth.constant andDevieWidth:width];
+    self.alcBtnOkheight.constant = [self getWidthOfTargetWithTargetValue:self.alcBtnOkheight.constant andDevieWidth:width];
+    self.alcIvBottmWidth.constant = [self getWidthOfTargetWithTargetValue:self.alcIvBottmWidth.constant andDevieWidth:width];
+    self.alcIvBottomHeight.constant = [self getWidthOfTargetWithTargetValue:self.alcIvBottomHeight.constant andDevieWidth:width];
+    self.alcLbInputCommentWidth.constant = [self getWidthOfTargetWithTargetValue:self.alcLbInputCommentWidth.constant andDevieWidth:width];
+    self.alcLbInputCommentHeight.constant = [self getWidthOfTargetWithTargetValue:self.alcLbInputCommentHeight.constant andDevieWidth:width];
+    self.alcLbPrivateServiceWidth.constant = [self getWidthOfTargetWithTargetValue:self.alcLbPrivateServiceWidth.constant andDevieWidth:width];
+    self.alcLbPrivateServiceHeight.constant = [self getWidthOfTargetWithTargetValue:self.alcLbPrivateServiceHeight.constant andDevieWidth:width];
     
-    [self setWidthOfTargetWithTargetValue:self.alcBottomMarginOfIvBottom.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcBottomMarginFromLbDetailToIvBottom.constant  andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcBottomMarginFromLbPrivateToLbDetail.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcBottomMarginFrombtnOkToLbPrivate.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcBottomMarginFromTfTobtnOk.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcBottomMarginFromLbInputToTf.constant andDevieWidth:width];
-    [self setWidthOfTargetWithTargetValue:self.alcBottomMarginFromIvLogoToLbInput.constant andDevieWidth:width];
+    self.alcBottomMarginOfIvBottom.constant = [self getWidthOfTargetWithTargetValue:self.alcBottomMarginOfIvBottom.constant andDevieWidth:width];
+    self.alcBottomMarginFromLbDetailToIvBottom.constant = [self getWidthOfTargetWithTargetValue:self.alcBottomMarginFromLbDetailToIvBottom.constant  andDevieWidth:width];
+    self.alcBottomMarginFromLbPrivateToLbDetail.constant = [self getWidthOfTargetWithTargetValue:self.alcBottomMarginFromLbPrivateToLbDetail.constant andDevieWidth:width];
+    self.alcBottomMarginFrombtnOkToLbPrivate.constant = [self getWidthOfTargetWithTargetValue:self.alcBottomMarginFrombtnOkToLbPrivate.constant andDevieWidth:width];
+    self.alcBottomMarginFromTfTobtnOk.constant = [self getWidthOfTargetWithTargetValue:self.alcBottomMarginFromTfTobtnOk.constant andDevieWidth:width];
+    self.alcBottomMarginFromLbInputToTf.constant = [self getWidthOfTargetWithTargetValue:self.alcBottomMarginFromLbInputToTf.constant andDevieWidth:width];
+    self.alcBottomMarginFromIvLogoToLbInput.constant = [self getWidthOfTargetWithTargetValue:self.alcBottomMarginFromIvLogoToLbInput.constant andDevieWidth:width];
     
     
-    self.lbInputComment.font = [UIFont systemFontOfSize: [self setWidthOfTargetWithTargetValue:16 andDevieWidth:width]];
-    self.lbPrivateService.font = [UIFont boldSystemFontOfSize: [self setWidthOfTargetWithTargetValue:14 andDevieWidth:width]];
-    self.lbDetail.font = [UIFont systemFontOfSize:[self setWidthOfTargetWithTargetValue:14 andDevieWidth:width]];
-    self.btnOk.titleLabel.font = [UIFont boldSystemFontOfSize:[self setWidthOfTargetWithTargetValue:15 andDevieWidth:width]];
+    self.lbInputComment.font = [UIFont systemFontOfSize: [self getWidthOfTargetWithTargetValue:16 andDevieWidth:width]];
+    self.lbPrivateService.font = [UIFont boldSystemFontOfSize: [self getWidthOfTargetWithTargetValue:14 andDevieWidth:width]];
+    self.lbDetail.font = [UIFont systemFontOfSize:[self getWidthOfTargetWithTargetValue:14 andDevieWidth:width]];
+    self.btnOk.titleLabel.font = [UIFont boldSystemFontOfSize:[self getWidthOfTargetWithTargetValue:15 andDevieWidth:width]];
     
     int screenHeight = [UIScreen mainScreen].bounds.size.height;
 
     if(screenHeight == 480)
     {
-        [self setHeightOfTargetWithTargetValue:self.alcBottomMarginFromLbDetailToIvBottom.constant andDeviceHeight:screenHeight];
-        [self setHeightOfTargetWithTargetValue:self.alcBottomMarginFromLbPrivateToLbDetail.constant andDeviceHeight:screenHeight];
-        [self setHeightOfTargetWithTargetValue:self.alcBottomMarginFrombtnOkToLbPrivate.constant andDeviceHeight:screenHeight];
-        [self setHeightOfTargetWithTargetValue:self.alcBottomMarginFromTfTobtnOk.constant andDeviceHeight:screenHeight];
-        [self setHeightOfTargetWithTargetValue:self.alcBottomMarginFromLbInputToTf.constant andDeviceHeight:screenHeight];
-        [self setHeightOfTargetWithTargetValue:self.alcBottomMarginFromIvLogoToLbInput.constant andDeviceHeight:screenHeight];
-        [self setHeightOfTargetWithTargetValue:self.alcBottomMarginOfIvBottom.constant  andDeviceHeight:screenHeight];
+        self.alcBottomMarginFromLbDetailToIvBottom.constant = [self getHeightOfTargetWithTargetValue:self.alcBottomMarginFromLbDetailToIvBottom.constant andDeviceHeight:screenHeight];
+        self.alcBottomMarginFromLbPrivateToLbDetail.constant = [self getHeightOfTargetWithTargetValue:self.alcBottomMarginFromLbPrivateToLbDetail.constant andDeviceHeight:screenHeight];
+        self.alcBottomMarginFrombtnOkToLbPrivate.constant = [self getHeightOfTargetWithTargetValue:self.alcBottomMarginFrombtnOkToLbPrivate.constant andDeviceHeight:screenHeight];
+        self.alcBottomMarginFromTfTobtnOk.constant = [self getHeightOfTargetWithTargetValue:self.alcBottomMarginFromTfTobtnOk.constant andDeviceHeight:screenHeight];
+        self.alcBottomMarginFromLbInputToTf.constant = [self getHeightOfTargetWithTargetValue:self.alcBottomMarginFromLbInputToTf.constant andDeviceHeight:screenHeight];
+        self.alcBottomMarginFromIvLogoToLbInput.constant = [self getHeightOfTargetWithTargetValue:self.alcBottomMarginFromIvLogoToLbInput.constant andDeviceHeight:screenHeight];
+        self.alcBottomMarginOfIvBottom.constant = [self getHeightOfTargetWithTargetValue:self.alcBottomMarginOfIvBottom.constant  andDeviceHeight:screenHeight];
 
 
     }
 
 }
 
--(CGFloat)setWidthOfTargetWithTargetValue:(CGFloat)width andDevieWidth:(CGFloat)dvWidth
+-(CGFloat)getWidthOfTargetWithTargetValue:(CGFloat)width andDevieWidth:(CGFloat)dvWidth
 {
     CGFloat result;
     
@@ -154,7 +154,7 @@
     
 }
 
--(CGFloat)setHeightOfTargetWithTargetValue:(CGFloat)height andDeviceHeight:(CGFloat)dvHeight
+-(CGFloat)getHeightOfTargetWithTargetValue:(CGFloat)height andDeviceHeight:(CGFloat)dvHeight
 {
     CGFloat result;
     
@@ -167,7 +167,7 @@
 
 
 #pragma mark - Set UIColor
--(UIColor *)setColorWithRGBCode:(NSString *)code
+-(UIColor *)getColorWithRGBCode:(NSString *)code
 {
     UIColor *color = nil;
     
@@ -176,36 +176,35 @@
     [rgbDictionary setObject:[code substringWithRange:NSMakeRange(2, 2)] forKey:@"Green"];
     [rgbDictionary setObject:[code substringWithRange:NSMakeRange(4, 2)] forKey:@"Blue"];
     
-    for (NSString *colorKey in rgbDictionary.allKeys) {
+    for (NSString *colorKey in rgbDictionary.allKeys)
+    {
         NSString *colorValue = [rgbDictionary objectForKey:colorKey];
-        NSString *color1 = [colorValue substringWithRange:NSMakeRange(0, 1)];
-        NSString *color2 = [colorValue substringWithRange:NSMakeRange(1, 1)];
-  
-        if([color1 isEqualToString:@"a"])
-            color1 = @"10";
-        else if([color1 isEqualToString:@"b"])
-            color1 = @"11";
-        else if([color1 isEqualToString:@"c"])
-            color1 = @"12";
-        else if([color1 isEqualToString:@"d"])
-            color1 = @"13";
-        else if([color1 isEqualToString:@"e"])
-            color1 = @"14";
-        else if([color1 isEqualToString:@"f"])
-            color1 = @"15";
+        NSString *color, *color1, *color2 = nil;
         
-        if([color2 isEqualToString:@"a"])
-            color2 = @"10";
-        else if([color2 isEqualToString:@"b"])
-            color2 = @"11";
-        else if([color2 isEqualToString:@"c"])
-            color2 = @"12";
-        else if([color2 isEqualToString:@"d"])
-            color2 = @"13";
-        else if([color2 isEqualToString:@"e"])
-            color2 = @"14";
-        else if([color2 isEqualToString:@"f"])
-            color2 = @"15";
+        for(int i = 0; i<2;i++)
+        {
+            
+            color = [colorValue substringWithRange:NSMakeRange(i, 1)];
+      
+            if([color isEqualToString:@"a"])
+                color = @"10";
+            else if([color isEqualToString:@"b"])
+                color = @"11";
+            else if([color isEqualToString:@"c"])
+                color = @"12";
+            else if([color isEqualToString:@"d"])
+                color = @"13";
+            else if([color isEqualToString:@"e"])
+                color = @"14";
+            else if([color isEqualToString:@"f"])
+                color = @"15";
+            
+            if(i == 0)
+                color1 = color;
+            else
+                color2 = color;
+            
+        }
         
         CGFloat colorValue1 = color1.floatValue;
         CGFloat colorValue2 = color2.floatValue;
