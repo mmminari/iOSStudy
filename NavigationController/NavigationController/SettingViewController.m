77fd4bf2b5730ec28dbdad9f3b902bffedf81c89
김민아 <mmminari@gimminaui-macbook-pro.local>
2016-06-15 14:40:19 +0900

    uibutton만든 후 눌렀을 때 log 출력

--- NavigationController/NavigationController/SettingViewController.m
+++ NavigationController/NavigationController/SettingViewController.m
@@ -74,9 +74,22 @@
     [self setColorAndFont];
     [self setLayout];
     
+    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(10.0f, 600.0f, 50.0f, 50.0f)];
+    [button setBackgroundColor:[UIColor purpleColor]];
+    [self.view addSubview:button];
+    [button addTarget:self action:@selector(touchedButton:) forControlEvents:UIControlEventTouchUpInside];
+
+    
 }
 
 #pragma mark - User Action
+
+-(void)touchedButton:(UIButton *)btn
+{
+    LogRed(@"button touched!");
+    
+}
+
 - (IBAction)touchedLogOut:(id)sender
 {
    // [self startSession];
